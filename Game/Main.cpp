9708116af1a1c0181cbaf680e85f261c9fa428a2
TWatson-Gamester <twatson@student.neumont.edu>
2021-08-10 16:33:32 -0600

    Added AudioChannel and AudioSystem

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -18,6 +18,11 @@ int main(int, char**)
 
 	gn::SetFilePath("../Resources");
 
+	engine.Get<gn::AudioSystem>()->AddAudio("explosion", "audio/explosion.wav");
+	engine.Get<gn::AudioSystem>()->AddAudio("Epic", "audio/Epic.mp3");
+	gn::AudioChannel channel = engine.Get<gn::AudioSystem>()->PlayAudio("Epic", 1, 1, true);
+
+
 	std::shared_ptr<gn::Texture> texture = engine.Get<gn::ResourceSystem>()->Get<gn::Texture>("sf2.png", engine.Get<gn::Renderer>());
 
 	for (size_t i = 0; i < 20; i++) {
@@ -49,13 +54,15 @@ int main(int, char**)
 
 			gn::Vector2 position = engine.Get<gn::InputSystem>()->GetMousePosition();
 			//Create Particles
-			scene.engine->Get<gn::ParticleSystem>()->Create(position, 1, 100.0f, engine.Get<gn::ResourceSystem>()->Get<gn::Texture>("particle01.png", engine.Get<gn::Renderer>()), 100);
+			scene.engine->Get<gn::ParticleSystem>()->Create(position, 50, 10.0f, engine.Get<gn::ResourceSystem>()->Get<gn::Texture>("particle01.png", engine.Get<gn::Renderer>()), 100.0f);
+			engine.Get<gn::AudioSystem>()->PlayAudio("explosion", 1, gn::RandomRange(.2f, 2.0f));
+			channel.SetPitch(channel.GetPitch() + .1f);
 
 		}
 		scene.Update(engine.time.deltaTime);
 
-		if (engine.time.time >= quitTime) quit = true;
-		engine.time.timeScale = 50000;
+		//if (engine.time.time >= quitTime) quit = true;
+		engine.time.timeScale = 10;
 
 		engine.Get<gn::Renderer>() -> BeginFrame();
 
